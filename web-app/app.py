ede583969ec56a9944048b382a3fabd65af7afca
sanabriageorge <gas9907@nyu.edu>
2026-04-14 14:03:11 -0400

    Basic Web App Setup

--- web-app/app.py
+++ web-app/app.py
@@ -1,9 +1,16 @@
+"""Flask web app for audio upload."""
+
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def index():
+    """Homepage."""
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
