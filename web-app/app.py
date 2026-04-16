8e4b2e5b51b31d8766234d0b0724e6e083314f79
OverYander <gavinchen8706@gmail.com>
2026-04-15 20:08:50 -0400

    adding a dependecies; connecting to mongodb; adding analyze route for file input

--- web-app/app.py
+++ web-app/app.py
@@ -1,15 +1,39 @@
 """Flask web app for sound-alert uploads and results."""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+import sys
+
+sys.stdout.reconfigure(line_buffering=True)
+load_dotenv()
+
+uri = os.getenv("MONGO_URI")
+print(uri)
+client = MongoClient(uri)
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    """Homepage."""
+    """Homepage"""
     return render_template("index.html")
 
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    """Send Audio recording to be analyzed"""
+    try:
+        print(request.files)
+        file = request.files["audiofile"]
+        print(file)
+        print(file.content_type)
+
+        return jsonify({"success": True})
+
+    except Exception as err:
+        print("err:", err)
+        return jsonify({"success": False, "error": str(err)}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
